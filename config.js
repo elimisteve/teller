bc3ff9b9f432daa2816ca1a2bae1e17e89309218
Steve Phillips <elimisteve@gmail.com>
2012-09-19 01:42:50 -0700

    Added (mostly) cleaned config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+{
+    "aj": {
+        "aliases": ["ajvb"],
+        "try_order": ["gtalk", "email", "sms", "cloakcast"],
+        "cloakcast": "aj@cloakcast.com",
+        "email": "ajvb.wkyhu@gmail.com",
+        "gtalk": "ajvb.wkyhu@gmail.com",
+        "sms": "(805) XXX-XXXX"
+    },
+    "steve": {
+        "aliases": ["elimisteve"],
+        "try_order": ["sms", "gtalk", "email", "cloakcast"],
+        "cloakcast": "steve@cloakcast.com",
+        "email": "elimisteve@gmail.com",
+        "gtalk": "elimisteve@gmail.com",
+        "sms": "(707) XXX-XXXX"
+    }
+}
